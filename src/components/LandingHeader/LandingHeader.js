8eb0e1d64f2336d9aa373a2df45815907b5de6dd
Neil Doshi <neil@nzymtechnologies.com>
2021-10-04 18:05:01 +0530

    fixing landing page css and layouting

--- src/components/LandingHeader/LandingHeader.js
+++ src/components/LandingHeader/LandingHeader.js
@@ -3,7 +3,19 @@ import Logo from '../Logo/logo';
 import Grid from '@mui/material/Grid';
 import FplButton from '../FplButton/FplButton';
 import star from '../../assets/star.svg';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    root:{
+        marginTop:'1.5rem'
+    }
+});
+
+
+
 export default function LandingHeader(props) {
+
+    const classes = useStyles();
 
     return (
         <Grid
@@ -11,6 +23,7 @@ export default function LandingHeader(props) {
             direction="row" 
             alignItems="center" 
             justifyContent="space-between"
+            className={classes.root}
         >
             <Logo></Logo>
             <FplButton label="Sign in / Sign up" hasIcon='false' icon={star} ></FplButton>
--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -2,27 +2,52 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import landingImg from '../../assets/landing-img.png';
-export default function LandingHeader(props) {
+import landingMainBg from '../../assets/landing-main-bg.svg';
+import { makeStyles } from '@mui/styles';
 
+const useStyles = makeStyles({
+    root: {
+        position: "relative"
+    },
+    bgImage: {
+        zIndex: -9,
+        position: 'absolute',
+        top: '20vh',
+        left:'-32vw'
+    },
+    container: {
+        display:'flex',
+        justifyContent:'space-around'
+    },
+    textSection: {
+        marginLeft: '-30vw',
+    },
+    imageSection: {
+        marginRight: '-30vw',
+    }
+});
+
+export default function LandingMain(props) {
+
+    const classes = useStyles();
     return (
-    <Grid container 
-        spacing={2}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        >
-        <Grid item xs={5}>
-            <h1>New to Investing ?</h1>
-            <ul>
-                <li>Learn to Invest by building your Portfolio</li>
-                <li>Analyse Stocks to make right decsion</li>
-                <li>Submit your portfolio and win rewards</li>
-            </ul>
-            <Button>Get Started</Button>
-        </Grid>
-        <Grid item xs={7}>
-            <img src={landingImg} alt="" srcset="" />
-        </Grid>
-    </Grid>);
+        <div className={classes.root}>
+            <img src={landingMainBg} alt="" srcset="" className={classes.bgImage} />
+            <div className={classes.container}>
+                <div className={classes.textSection}>
+                    <h1>New to Investing ?</h1>
+                    <ul>
+                        <li>Learn to Invest by building your Portfolio</li>
+                        <li>Analyse Stocks to make right decsion</li>
+                        <li>Submit your portfolio and win rewards</li>
+                    </ul>
+                    <Button>Get Started</Button>
+                </div>
+                <div className={classes.imageSection}>
+                    <img src={landingImg} alt="" srcset="" />
+                </div>
+            </div>
+        </div>
+    );
 
 }
