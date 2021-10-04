d78d7749de2a2864bf5ad9be9ce9c4d23a7c4c1c
bpatro <bpatro@motorola.com>
2021-10-04 19:54:19 +0530

    removed background image.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import styled from "styled-components";
 import Login from "./components/Login/Login";
 import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
-import backgroundImage from "./assets/Background.png";
 
 function App() {
-  const MainWrapper = styled.div`
-    background: #ffffff url(${backgroundImage}) no-repeat;
-    background-size: cover;
-    min-height: 600px;
-
-    @media (min-width: 768px) {
-      min-height: 1000px;
-    }
-  `;
   return (
-    <MainWrapper>
+    <div>
       <Switch>
         <Route exact path="/">
           <Landing />
@@ -29,7 +18,7 @@ function App() {
           <Home />
         </Route>
       </Switch>
-    </MainWrapper>
+    </div>
   );
 }
 
