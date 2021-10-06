15c50e074916411e76f269670b8a82c6ee9b8627
Neil Doshi <neil@nzymtechnologies.com>
2021-10-06 15:55:00 +0530

    added sentry for tracking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+// production error logging 
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+
+
+Sentry.init({
+  dsn: "https://796471a0672747a6b249efe6a2ccda0d@o1028015.ingest.sentry.io/5995121",
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
