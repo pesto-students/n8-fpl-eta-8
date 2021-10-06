39c83726fc2e33ffb0467909bd3796f5c6893f01
Neil Doshi <neil@nzymtechnologies.com>
2021-10-06 23:31:55 +0530

    scafold created

--- src/components/ChallengeList/ChallengeList.js
+++ src/components/ChallengeList/ChallengeList.js
@@ -4,12 +4,33 @@ import React from "react";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
+// styling 
+import { useStyles } from "./styles";
+
 export default function ChallengeList() {
-  return (
-    <Container>
-        <Grid container direction="column" alignItems="center">
-            The Challenge List Page goes here
-        </Grid>
-    </Container>
-  );
+
+    const classes = useStyles();
+
+    return (
+        <Container>
+            <Grid container direction="column" alignItems="center">
+                <Grid container direction="row" justifyContent="space-between" >
+                    <Grid item xs={12} md={3}>
+                        Filter Challenges 
+                    </Grid>
+                    <Grid item xs={12} md={9}>
+                        BSE sensex data here
+                    </Grid>
+                </Grid>
+                <Grid container direction="row" >
+                    <Grid item xs={0} md={3}>
+                        empty
+                    </Grid>
+                    <Grid item xs={12} md={9}>
+                        challenge list
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
