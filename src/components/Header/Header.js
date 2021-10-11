b850a561e3a179056e52a83d8f7cb8e2c67935bb
Neil Doshi <neil@nzymtechnologies.com>
2021-10-11 18:43:16 +0530

    base template set

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,73 @@ import React from 'react';
 // mui components
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { makeStyles } from '@mui/styles';
 
 // custom components
 import Logo from '../Logo/logo'
 
+const useStyles = makeStyles({
+    profileIcon: {},
+    toolbar:{
+        display:'flex',
+        justifyContent:'space-between'
+    },
+});
+
+
 export default function Header() {
+
+    const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <AppBar>
-            <Toolbar>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
                 <Logo light />
+                <div>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                        className={classes.profileIcon}
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                    </Menu>
+                </div>
             </Toolbar>
         </AppBar>
     );
