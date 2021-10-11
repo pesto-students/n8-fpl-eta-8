37659cf5d1e34727114603b3959c189424cc5784
Neil Doshi <neil@nzymtechnologies.com>
2021-10-11 18:51:29 +0530

    avatar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 // mui components
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import { makeStyles } from '@mui/styles';
@@ -14,9 +13,9 @@ import Logo from '../Logo/logo'
 
 const useStyles = makeStyles({
     profileIcon: {},
-    toolbar:{
-        display:'flex',
-        justifyContent:'space-between'
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between'
     },
 });
 
@@ -40,17 +39,15 @@ export default function Header() {
             <Toolbar className={classes.toolbar}>
                 <Logo light />
                 <div>
-                    <IconButton
-                        size="large"
+                    <Avatar
+                        alt="Neil Doshi"
+                        src="https://lh3.googleusercontent.com/a/AATXAJynsfX2I1dcAN4Jh4QT7oxPezlIretDoipIIYln=s96-c"
                         aria-label="account of current user"
                         aria-controls="menu-appbar"
                         aria-haspopup="true"
                         onClick={handleMenu}
-                        color="inherit"
                         className={classes.profileIcon}
-                    >
-                        <AccountCircle />
-                    </IconButton>
+                    />
                     <Menu
                         id="menu-appbar"
                         anchorEl={anchorEl}
@@ -67,7 +64,7 @@ export default function Header() {
                         onClose={handleClose}
                     >
                         <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
                     </Menu>
                 </div>
             </Toolbar>
