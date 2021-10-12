8f8b02807232a44a83758eb9e00f72492afdf0c6
Neil Doshi <neil@nzymtechnologies.com>
2021-10-12 13:41:28 +0530

    Leader Board in Progress

--- src/components/LeaderBoard/LeaderBoard.jsx
+++ src/components/LeaderBoard/LeaderBoard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import { Card, Typography } from '@mui/material'
+import { Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 
 import { useStyles } from '../LeaderBoard/styles'
 
 export default function LeaderBoard(props) {
 
     const classes = useStyles();
+
     return (
         <>
             <Card
+                variant="outlined"
                 className={classes.root}
             >
                 <Typography
@@ -17,6 +19,47 @@ export default function LeaderBoard(props) {
                     className={classes.title}>
                     Leader Board
                 </Typography>
+                <TableContainer>
+                    <Table
+                        sx={{ minWidth: 320 }}
+                        aria-label="table title"
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className={classes.tableTitle}>Participant</TableCell>
+                                <TableCell className={classes.tableTitle} align="right">Average Return</TableCell>
+                                <TableCell className={classes.tableTitle}>1 Day Change</TableCell>
+                                <TableCell className={classes.tableTitle}>1 Day Rank Change</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow className={classes.tableRow}>
+                                <TableCell className={classes.tableCell}>1. Tushar L.</TableCell>
+                                <TableCell className={classes.tableCell} align="right"> 05.67% </TableCell>
+                                <TableCell className={classes.tableCell}>+ 9.4%</TableCell>
+                                <TableCell className={classes.tableCell}>2 postion Up</TableCell>
+                            </TableRow>
+                            <TableRow className={classes.tableRow}>
+                                <TableCell className={classes.tableCell}>1. Tushar L.</TableCell>
+                                <TableCell className={classes.tableCell} align="right"> 05.67% </TableCell>
+                                <TableCell className={classes.tableCell}>+ 9.4%</TableCell>
+                                <TableCell className={classes.tableCell}>2 postion Up</TableCell>
+                            </TableRow>
+                            <TableRow className={classes.tableRow}>
+                                <TableCell className={classes.tableCell}>1. Tushar L.</TableCell>
+                                <TableCell className={classes.tableCell} align="right"> 05.67% </TableCell>
+                                <TableCell className={classes.tableCell}>+ 9.4%</TableCell>
+                                <TableCell className={classes.tableCell}>2 postion Up</TableCell>
+                            </TableRow>
+                            <TableRow className={classes.tableRow}>
+                                <TableCell className={classes.tableCell}>1. Tushar L.</TableCell>
+                                <TableCell className={classes.tableCell} align="right"> 05.67% </TableCell>
+                                <TableCell className={classes.tableCell}>+ 9.4%</TableCell>
+                                <TableCell className={classes.tableCell}>2 postion Up</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Card>
         </>
     )
