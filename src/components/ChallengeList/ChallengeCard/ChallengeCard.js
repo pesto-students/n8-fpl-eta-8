7a89f76b6c38ec8e46a956bbb80f3ba2257aba04
Neil Doshi <neil@nzymtechnologies.com>
2021-10-14 17:24:09 +0530

    fix date param on Challenge Card

--- src/components/ChallengeList/ChallengeCard/ChallengeCard.js
+++ src/components/ChallengeList/ChallengeCard/ChallengeCard.js
@@ -6,24 +6,24 @@ import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
 
 import { useStyles } from "./styles";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import { Timestamp } from 'firebase/firestore';
 
 export default function ChallengeCard(props) {
   const classes = useStyles();
-  const { name, id, startDate, endDate} = props.challenge;
+  const { name, id, startDate, endDate } = props.challenge;
 
-  let s_date = new Date(1970, 0, 1);
-  s_date.setSeconds(startDate._seconds);
 
-  let e_date = new Date(1970, 0, 1);
-  e_date.setSeconds(endDate._seconds);
+  let s_date = new Timestamp(startDate._seconds, startDate._nanoseconds);
+  let e_date = new Timestamp(endDate._seconds, endDate._nanoseconds);
 
   return (
     <Card variant="outlined" className={classes.root}>
       <div className={classes.challengeDetails}>
         <Typography variant="h5">{name}</Typography>
         <Typography variant="h6">
-          {s_date.toDateString()} - {e_date.toDateString()}
+          {s_date.toDate().toDateString()} - {e_date.toDate().toDateString()}
         </Typography>
       </div>
       <Link className={classes.styledLink} to={`/challenge/${id}`}>
