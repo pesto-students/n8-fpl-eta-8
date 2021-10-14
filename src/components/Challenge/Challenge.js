2ff95fa2a098b8784b352916cd5df1d2f141fd81
bpatro <bpatro@motorola.com>
2021-10-14 15:45:11 +0530

    Refresh Button along with demo login.

--- src/components/Challenge/Challenge.js
+++ src/components/Challenge/Challenge.js
@@ -2,33 +2,36 @@ import React from "react";
 
 // mui
 import Container from "@mui/material/Container";
-import { Grid, Typography } from "@mui/material";
-
-
+import { Button, Grid, Typography } from "@mui/material";
+import UpdateIcon from "@mui/icons-material/Update";
 
 // Custom Components
 import ChallengeStatus from "../ChallengeStatus/ChallengeStatus";
 import Portfolio from "../Portfolio/Portfolio";
 
-
 import { useStyles } from "./styles";
 import LeaderBoard from "../LeaderBoard/LeaderBoard";
 
 export default function Challenge() {
-
   const classes = useStyles();
   return (
     <>
       <Container>
-        <Typography
-          variant="h4"
-          className={classes.challengeTitle}>
+        <Typography variant="h4" className={classes.challengeTitle}>
           Challenge Name
+          <div className={classes.updateDetails}>
+            <span className={classes.updateDetailsText}>Last Updated at</span>
+            <Button
+              className={classes.refreshButton}
+              aria-label="refresh"
+              size="large"
+              variant="contained"
+            >
+              <UpdateIcon fontSize="medium" />
+            </Button>
+          </div>
         </Typography>
-        <Grid
-          container
-          direction="row"
-          spacing={2}>
+        <Grid container direction="row" spacing={2}>
           <Grid item xs={12} md={10} lg={9}>
             <Portfolio />
           </Grid>
