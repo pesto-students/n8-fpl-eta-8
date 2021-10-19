f8bdf1d65ee844e15f6c49b8313b4530f49a7760
bpatro <bpatro@motorola.com>
2021-10-19 22:11:08 +0530

    Update profile with password change feature completed without form validation.

--- src/components/AccountOverview/AccountOverviewStyle.js
+++ src/components/AccountOverview/AccountOverviewStyle.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const AccountOverviewWrapper = styled(Card)`
   margin-top: 16px;
+  height: calc(100% - 177px);
 `;
 
 export const AccountOverviewTitle = styled.div`
