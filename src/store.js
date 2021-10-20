3c326048501f046f267db377ded4d8dbdaf8d6a7
Neil Doshi <neil@nzymtechnologies.com>
2021-10-20 16:54:21 +0530

    added persist layer to redux

--- src/store.js
+++ src/store.js
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+
+
+// store-feature 
 import { leaderboardView } from './store-features/leaderboardView'
 import { user } from './store-features/user'
 
+const reducers = combineReducers({
+  user: user.reducer,
+  leaderboardView: leaderboardView.reducer
+});
+
+const persistConfig = {
+  key:'root',
+  storage
+}
+
+const persistedReducers = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    user: user.reducer,
-    leaderboardView:leaderboardView.reducer
-  },
+  reducer: persistedReducers,
+  devTools:process.env.NODE_ENV !== 'production'
 })
