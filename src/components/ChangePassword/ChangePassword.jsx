de101667464ae8f42a15c8781421e0d7e2d092d4
bpatro <bpatro@motorola.com>
2021-10-22 19:45:58 +0530

    resolved console errors and implementation of snackbar.

--- src/components/ChangePassword/ChangePassword.jsx
+++ src/components/ChangePassword/ChangePassword.jsx
@@ -10,20 +10,27 @@ import {
 } from "./ChangePasswordStyle";
 import firebase from "../../firebase";
 import { useSelector } from "react-redux";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Changepassword() {
   const user = useSelector((state) => state.user);
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [open, setOpen] = React.useState(false);
 
   async function updatePassword() {
-    console.log(oldPassword);
     try {
       await firebase.reLogin(user.email, oldPassword).then(() => {
         if (newPassword.length > 0 && newPassword === confirmPassword) {
           firebase.setNewPassword(newPassword).then(() => {
             console.log("Password Updated Successfully");
+            setOpen(true);
           });
         }
       });
@@ -70,6 +77,11 @@ export default function Changepassword() {
           }}
         />
       </CardContent>
+      <Snackbar open={open} autoHideDuration={6000}>
+        <Alert severity="success" sx={{ width: "100%" }}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </ChangePasswordwWrapper>
   );
 }
