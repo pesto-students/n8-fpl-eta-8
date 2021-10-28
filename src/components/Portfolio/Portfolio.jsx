27538bd9742cf8aaf2dc1437bd487e3be232a755
bpatro <bpatro@motorola.com>
2021-10-28 13:20:48 +0530

    initial commit.

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,7 +6,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import StockPicker from "../StockPicker/StockPicker";
+import StockPicker from "components/StockPicker/StockPicker";
 
 const PortfolioCard = styled(Card)`
   border-radius: 12px !important;
