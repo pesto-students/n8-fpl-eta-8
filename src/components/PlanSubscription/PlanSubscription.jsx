d38ee91c6294a6f391cd7b094dc290e0f5ee410c
bpatro <bpatro@motorola.com>
2021-10-29 09:44:31 +0530

    Subscription Plan

--- src/components/PlanSubscription/PlanSubscription.jsx
+++ src/components/PlanSubscription/PlanSubscription.jsx
@@ -1,33 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Card, CardContent } from "@mui/material";
 import {
   Amount,
+  BackButton,
+  Badge,
   Currency,
   PlanDesc,
   PlanName,
-  PlanSubscriptioTitle,
+  PlanSubscriptionTitle,
+  SelectPlanTitle,
   TimeSpan,
   UpgradeButton,
 } from "./PlanSubscriptioStyle";
 
 export default function PlanSubscription() {
+  const [isPlanViewed, setIsPlanViewed] = useState(false);
   return (
     <>
-      <PlanSubscriptioTitle>Plan Subscription</PlanSubscriptioTitle>
-      <UpgradeButton>Upgrade</UpgradeButton>
-      <Grid item xs={12} sm={5} md={5} lg={5}>
-        <Card>
-          <CardContent>
-            <div>
-              <Currency>Rs</Currency> <Amount>0</Amount>
-              <TimeSpan>/per month</TimeSpan>
-            </div>
-            <PlanName>Basic</PlanName>
-            <PlanDesc>To familiarize yourself with our tools</PlanDesc>
-            <PlanDesc>Max. 1 Challenge per Month</PlanDesc>
-          </CardContent>
-        </Card>
-      </Grid>
+      {isPlanViewed ? (
+        <>
+          <SelectPlanTitle>
+            Select Plan
+            <BackButton onClick={() => setIsPlanViewed(false)} />
+          </SelectPlanTitle>
+          <Grid container direction="row" spacing={2}>
+            <Grid item xs={12} sm={6} md={5} lg={5}>
+              <Card>
+                <CardContent>
+                  <div>
+                    <Currency>Rs</Currency> <Amount>499</Amount>
+                    <TimeSpan>/per month</TimeSpan>
+                    <UpgradeButton>Select</UpgradeButton>
+                  </div>
+                  <PlanName>Premium</PlanName>
+                  <PlanDesc>Do detail strategy and stock analysis </PlanDesc>
+                  <PlanDesc>
+                    <Badge />
+                    Max. 5 Challenge per Month
+                  </PlanDesc>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={5} lg={5}>
+              <Card>
+                <CardContent>
+                  <div>
+                    <Currency>Rs</Currency> <Amount>999</Amount>
+                    <TimeSpan>/per month</TimeSpan>
+                    <UpgradeButton>Select</UpgradeButton>
+                  </div>
+                  <PlanName>Ultra</PlanName>
+                  <PlanDesc>Do detail strategy and stock analysis </PlanDesc>
+                  <PlanDesc>
+                    <Badge />
+                    Max. 10 Challenge per Month
+                  </PlanDesc>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <>
+          <PlanSubscriptionTitle>Plan Subscription</PlanSubscriptionTitle>
+          <UpgradeButton onClick={() => setIsPlanViewed(true)}>
+            Upgrade
+          </UpgradeButton>
+          <Grid item xs={12} sm={5} md={5} lg={5}>
+            <Card>
+              <CardContent>
+                <div>
+                  <Currency>Rs</Currency> <Amount>0</Amount>
+                  <TimeSpan>/per month</TimeSpan>
+                </div>
+                <PlanName>Basic</PlanName>
+                <PlanDesc>To familiarize yourself with our tools</PlanDesc>
+                <PlanDesc>
+                  <Badge />
+                  Max. 1 Challenge per Month
+                </PlanDesc>
+              </CardContent>
+            </Card>
+          </Grid>
+        </>
+      )}
     </>
   );
 }
