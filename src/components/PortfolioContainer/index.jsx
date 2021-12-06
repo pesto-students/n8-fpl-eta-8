f97e0eacc4689a32a8d2fc3d3f1b08275e69f1cf
Neil Doshi <neil@nzymtechnologies.com>
2021-12-06 16:16:06 +0530

    redo portfilio component

---
+++ src/components/PortfolioContainer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioContainer = () => {
+    return (
+        <div>
+                                        
+        </div>
+    );
+}
+
+export default PortfolioContainer;
